13b4849d145ef841e42679da946e186222ce8a5c
Diego Nascimento <69281579+diegodevts@users.noreply.github.com>
2024-06-02 13:44:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('viewProjects').addEventListener('click', function(event) {
     event.preventDefault();
     const projectsSection = document.getElementById('projects');
-    projectsSection.scrollIntoView({ behavior: 'smooth' });
+    projectsSection.scrollIntoView({ behavior: 'smooth', block: 'start' }); // Alteração na velocidade da transição
 });
