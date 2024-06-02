5013c1a2fe6fa7024ea19e428742b19d02a073d2
Diego Nascimento <69281579+diegodevts@users.noreply.github.com>
2024-06-02 13:34:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-const sections = document.querySelectorAll('section');
-
-function scrollAppear() {
-    sections.forEach(section => {
-        const sectionPosition = section.getBoundingClientRect().top;
-        const screenPosition = window.innerHeight / 1.3;
-
-        if (sectionPosition < screenPosition) {
-            section.classList.add('visible');
-        }
-    });
-}
-
-window.addEventListener('scroll', scrollAppear);
+document.getElementById('viewProjects').addEventListener('click', function(event) {
+    event.preventDefault();
+    const projectsSection = document.getElementById('projects');
+    projectsSection.scrollIntoView({ behavior: 'smooth' });
+});
